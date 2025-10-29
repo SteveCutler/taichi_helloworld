8a8d849de6c6c383ca4c70eb508f89deb638598f
SteveCutler <stephen.c.cutler@gmail.com>
2025-10-29 12:04:02 -0400

    wrote diffuse kernel, reorganized

--- cfd.py
+++ cfd.py
@@ -7,7 +7,11 @@
 
 import taichi as ti
 
+###########
 ## CONFIG
+##########
+
+##use CPU for debugging with print
 ti.init(arch=ti.cpu)
 
 N = 300 #field measurments
@@ -28,10 +32,14 @@ dt = 1/48 #timestep
 
 diff = 0.0001 #diffusion coefficient
 
-
 ## potential to add viscosity here
 
-@ti.func
+
+###########
+## HELPERS
+###########
+
+@ti.kernel
 def initialize_fields():
     
     for i,j in density:
@@ -48,20 +56,23 @@ def initialize_fields():
         density_prev[i,j] = ti.sin(i/N*1.0) * ti.cos(j/N*1.0)
         
         
-    ## create random curl velocity in initial vel fields
-    ## set density = 1 everywhere
-    
+@ti.Kernel
+def copy_field(field1: ti.template(), field2: ti.template()):
+    for I in ti.grouped(field1):
+        field1[I] = field2[I]
 
-    
 
-## TO DO: ti.kernels ##
+###########
+## FORCES 
+###########
+
+
 
 ## ADD FORCE
     #NOTES:
     #w1(x) = w0(x) + delta T * f(x,t)
     #frame 2 vel = frame 1 vel + the force * time interval
-
-@ti.func
+@ti.kernel
 def force():
     for I in density:
         ## adding gravity force * units per timestep to vertical velocity
@@ -76,8 +87,7 @@ def force():
     #basically frame 2 particle vel = tracing vel of particle backwards to previous time step, 
     # sampling vel from that x,y coordinate, interpolate
     # multiply by some diminishing fraction so doesn't blow up? * 0.95?
-
-@ti.func
+@ti.kernel
 def advect(u, v, density, u_prev, v_prev, density_prev, N, dt):
     ##grab the vertical and horizontal vector from the previous frame
     for i, j in density:
@@ -117,38 +127,65 @@ def advect(u, v, density, u_prev, v_prev, density_prev, N, dt):
         tr_v = v_prev[x_r,y_t]
 
         ## bilinear interpolation
-                  ## bottom_left         bottom right      top right      top left
+                      ## bottom_left            bottom right         top right        top left
         density[i,j] = (1-s_x)*(1-s_y)*bl_d + (s_x*(1-s_y)*br_d) + s_x*s_y*tr_d + (1-s_x)*s_y*tl_d
         u[i,j] = (1-s_x)*(1-s_y)*bl_u + (s_x*(1-s_y)*br_u) + s_x*s_y*tr_u + (1-s_x)*s_y*tl_u
         v[i,j] = (1-s_x)*(1-s_y)*bl_v + (s_x*(1-s_y)*br_v) + s_x*s_y*tr_v + (1-s_x)*s_y*tl_v
        
 
 
-@ti.kernel
-def main():
-    
-    initialize_fields()
-    advect(u, v, density, u_prev, v_prev, density_prev, N)
-
-main()
-
 ## DIFFUSE
+    ## NOTES: use gauss seidl relaxation, 20 iteration
     # w1(x,y) = w0(x,y) + a( w1(x-1,y) + w1(x+1,y) + w1(x,y-1) + w1(x,y+1)) / 1 + 4a
     #where a = diffusion coeff
 
     # gauss seidl interpolation, 20 iterations
     # calculate diffusion by averaging velocity/density for a given cell between the 4 or 8 neighbours, 
-    # reusing the new values for the next iteration
     # note: use the new values for the current iteration when available (the cells that came before)
+
+    @ti.kernel
+    def diffuse():
+        ##Diffuse using gauss seidl, 20 iterations
+
+        for i in range(20):
+            for i, j in density:
+                ## don't diffuse on boundaries
+                if( i == 0 or i == N-1 or j == 0 or j == N-1 ):
+                    continue
+                density[i,j] = density_prev[i,j] + (diff*(density[i-1,j]+density[i,j-1]+ density_prev[i+1,j] + density_prev[i,j+1]))/(1+4*diff)
+
+            copy_field(density_prev, density)
+            
+            ##enforce boundaries constraints with set_bnd
+
 
 ## PROJECT
     # poisson solver?
     # gauss seidl
 
 ## BOUNDARY CONDITIONS
+    ## create set bnd helper for setting boundary pixel values per stam
+    ##diffuse
+
+## TO DO:
+## min_max helper function
+## gauss_seidl helper function
+## set_bnd function
+## create random curl velocity in initial vel fields
+## render logic
 
 
-## RENDER
+
+
+
+@ti.kernel
+def main():
+    
+    initialize_fields()
+    force()
+    advect(u, v, density, u_prev, v_prev, density_prev, N)
+
+main()
 
 #### RENDER ######
 
@@ -164,7 +201,6 @@ main()
 # camera = ti.ui.Camera()
 # #start time
 # current_t = 0.0
-
 
 ##Render loop
 ##while window.running:
